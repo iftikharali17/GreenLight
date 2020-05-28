587fc0806d043bfcbce8601865409351c87d7198
Mad Ting <samtastic1337@gmail.com>
2020-05-28 16:34:34 +0200

    first real commit

--- App.js
+++ App.js
@@ -4,15 +4,22 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Shits gone real. </Text>
+      <Text style={green.colors}> Shits gone real. </Text>
+      <Text style={green.colors}> wHere you at Ifti </Text>
     </View>
   );
 }
 
+const green = StyleSheet.create({
+  colors: {
+    color: "rgb(50, 255, 40)",
+  },
+});
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
   },
