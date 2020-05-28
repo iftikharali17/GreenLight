0a89052824330d6d5b136e454a010192db274e5f
Mad Ting <samtastic1337@gmail.com>
2020-05-28 16:02:40 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Shits gone real </Text>
     </View>
   );
 }
@@ -12,8 +12,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "#00ff00",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
