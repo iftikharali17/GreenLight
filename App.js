024971c2c10e0410dcc6ad8cc866538b415fd92a
Mad Ting <samtastic1337@gmail.com>
2020-05-28 16:06:10 +0200

    frst commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Shits gone real </Text>
+      <Text> Shits gone real. </Text>
     </View>
   );
 }
