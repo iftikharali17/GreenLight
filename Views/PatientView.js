2c9c41d297c92c70eb34e76143d06aa7e4c24848
Mad Ting <samtastic1337@gmail.com>
2020-06-08 18:13:41 +0200

    Minor changes

---
+++ Views/PatientView.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
-import { SafeAreaView, View, FlatList, StyleSheet, Text } from "react-native";
 import axios from "axios";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet,
+  FlatList,
+} from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
 const LandingPage = () => {
+  const [doctors, doctorData] = useState();
+  useEffect(() => {
+    axios
+      .get("http://localhost:2000/all")
+
+      .then(function (response) {
+        // handle success
+        doctorData(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  });
+
   return (
     <View>
       <Text>Hello you are welcome</Text>
